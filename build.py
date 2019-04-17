261ed1343a16784bc042c99b9a076f56da0be4ef
daixian <amano_tooko@qq.com>
2019-04-18 00:56:00 +0800

    带中文编码失败..

--- build.py
+++ build.py
@@ -9,19 +9,19 @@ dirConcur = "./concurrentqueue"
 if not os.path.exists(dirConcur):
     os.makedirs(dirConcur)
 
-print("下载 concurrentqueue ...")
+print("download concurrentqueue ...")
 url = 'https://github.com/cameron314/concurrentqueue/raw/master/blockingconcurrentqueue.h'
 request.urlretrieve(url, dirConcur + "/blockingconcurrentqueue.h")
 url = "https://github.com/cameron314/concurrentqueue/raw/master/concurrentqueue.h"
 request.urlretrieve(url, dirConcur + "/concurrentqueue.h")
-print("下载完成")
+print("done!")
 
 #临时下载文件夹
 dirDownload = "./download"
 if not os.path.exists(dirDownload):
     os.makedirs(dirDownload)
 
-print("下载 spdlog ...")
+print("download spdlog ...")
 url = "https://github.com/gabime/spdlog/archive/v1.3.1.tar.gz"
 request.urlretrieve(url, dirDownload + "/v1.3.1.tar.gz")
-print("下载完成")
+print(""done!")
