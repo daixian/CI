a00d94265985a821c339441ca16c8e2a6e98e947
daixian <amano_tooko@qq.com>
2019-04-18 00:13:16 +0800

    实验

--- build.py
+++ build.py
@@ -2,13 +2,19 @@ import urllib
 from urllib import request
 import os
 
-dirConcur = "./concurrentqueue/"
+#下载
+dirConcur = "./concurrentqueue"
 if not os.path.exists(dirConcur):
     os.makedirs(dirConcur)
 
-print("下载...")
+print("下载 concurrentqueue ...")
 url = 'https://github.com/cameron314/concurrentqueue/raw/master/blockingconcurrentqueue.h'
-request.urlretrieve(url, "./concurrentqueue/blockingconcurrentqueue.h")
+request.urlretrieve(url, dirConcur + "/blockingconcurrentqueue.h")
 url = "https://github.com/cameron314/concurrentqueue/raw/master/concurrentqueue.h"
-request.urlretrieve(url, "./concurrentqueue/concurrentqueue.h")
+request.urlretrieve(url, dirConcur + "/concurrentqueue.h")
 print("下载完成")
+
+
+print("下载 spdlog ...")
+url = "https://github.com/gabime/spdlog/archive/v1.3.1.tar.gz"
+request.urlretrieve(url, dirConcur + "/blockingconcurrentqueue.h")
