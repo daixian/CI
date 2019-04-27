3deb5fc3cc0a4ecb0c2f8ccc953547c9de8b6726
daixian <amano_tooko@qq.com>
2019-04-27 22:52:32 +0800

    初步整理了下工程

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,15 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+#include "dlog/dlog.h"
+
+//#pragma execution_character_set("GB2312") 这个头定义不需要,但是要注意定义bom头
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    dlog_init("log", "main", dlog_init_relative::MODULE);
+    for (size_t i = 0; i < 10; i++) {
+        LogI("log %d", i);
+        LogI("输出一条日志 %d", i);
+    }
+
     return 0;
 }
