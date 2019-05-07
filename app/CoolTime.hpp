24e17563381142704c84757a404861d20b27ec6e
daixian <amano_tooko@qq.com>
2019-05-08 01:14:50 +0800

    重新进行了一下实验

---
+++ app/CoolTime.hpp
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include <time.h>
+
+namespace dxlib {
+///-------------------------------------------------------------------------------------------------
+/// <summary> 类似游戏的冷却时间的判断. </summary>
+///
+/// <remarks> Dx, 2018/4/10. </remarks>
+///-------------------------------------------------------------------------------------------------
+class CoolTime
+{
+  public:
+    CoolTime(float cd)
+    {
+        _lastTime = clock();
+        CD = cd;
+        isStart = true;
+        count = 0;
+    }
+
+    CoolTime()
+    {
+        _lastTime = clock();
+        CD = 1; //默认1秒
+        isStart = true;
+        count = 0;
+    }
+
+    ~CoolTime()
+    {
+    }
+
+    /// <summary> CD时间，以秒计算. </summary>
+    float CD;
+
+    /// <summary> 是否开启了冷却计时. </summary>
+    bool isStart;
+
+    /// <summary> cd结束的次数. </summary>
+    int count;
+
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// 是否CD结束，可以刷新了。
+    /// </summary>
+    ///
+    /// <remarks> Dx, 2017/9/4. </remarks>
+    ///
+    /// <returns> True if can flush, false if not. </returns>
+    ///-------------------------------------------------------------------------------------------------
+    bool isDone()
+    {
+        clock_t now = clock();
+        float costTime = (float)(now - _lastTime) / CLOCKS_PER_SEC;
+        if (costTime > CD || count == 0) //如果经过了（CD时间）秒,或者是初始状态
+        {
+            _lastTime = now; //记录现在的最近一次的时间
+            count++;
+            return true;
+        }
+        else
+            return false;
+    }
+
+  private:
+    clock_t _lastTime;
+};
+} // namespace dxlib
--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,7 @@
 #include "dlog/dlog.h"
 #include "opencv2/opencv.hpp"
 #include "concurrentqueue/concurrentqueue.h"
+#include "CoolTime.hpp"
 
 //#pragma execution_character_set("GB2312") 这个头定义不需要,但是要注意定义bom头
 
@@ -15,41 +16,58 @@ int main(int argc, char **argv)
     }
 
     moodycamel::ConcurrentQueue<std::string> queue[4];
-    long long count = 0;
+    long long count[4] = {0, 0, 0, 0};
+    dxlib::CoolTime ct[5];
+    for (size_t i = 0; i < 5; i++) {
+        ct[i].CD = 10;
+    }
+
+    LogI("开始执行并行实验！");
 #pragma omp parallel sections //并行区域
     {
+#pragma omp section //并行5 （实际实验count的增长是每秒2W8次左右,开始的时候能启动5核，后来只能启动4核）
+        while (true) {
+            for (size_t i = 0; i < 4; i++) {
+                std::string msg;
+                if (queue[i].try_dequeue(msg)) {
+                    count[i]++;
+                    if (ct[4].isDone()) {
+                        printf("count ={%lld, %lld, %lld, %lld}\r\n", count[0], count[1], count[2], count[3]);
+                    }
+                }
+            }
+        }
 #pragma omp section //并行1
         while (true) {
+            if (ct[0].isDone()) {
+                LogI("并行1 执行...");
+            }
             if (queue[0].size_approx() == 0)
                 queue[0].enqueue("section0");
         }
 #pragma omp section //并行2
         while (true) {
+            if (ct[1].isDone()) {
+                LogI("并行2 执行...");
+            }
             if (queue[1].size_approx() == 0)
                 queue[1].enqueue("section1");
         }
 #pragma omp section //并行3
         while (true) {
+            if (ct[2].isDone()) {
+                LogI("并行3 执行...");
+            }
             if (queue[2].size_approx() == 0)
                 queue[2].enqueue("section2");
         }
 #pragma omp section //并行4
         while (true) {
+            if (ct[3].isDone()) {
+                LogI("并行4 执行...");
+            }
             if (queue[3].size_approx() == 0)
                 queue[3].enqueue("section3");
-        }
-#pragma omp section //并行5 （实际实验count的增长是每秒100W次左右）
-        while (true) {
-            for (size_t i = 0; i < 4; i++) {
-                std::string msg;
-                if (queue[i].try_dequeue(msg)) {
-                    count++;
-                    if (count % 1000000 == 0) {
-                        printf("count = %lld\r\n", count);
-                    }
-                }
-                //printf("%s\r\n",msg.c_str());
-            }
         }
     }
     return 0;
