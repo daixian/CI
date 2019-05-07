56f62fefee34da147fe0ce443abb853f61719661
daixian <amano_tooko@qq.com>
2019-05-08 00:37:46 +0800

    试了一下多核并发读写队列

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <cstdio>
 #include "dlog/dlog.h"
 #include "opencv2/opencv.hpp"
+#include "concurrentqueue/concurrentqueue.h"
 
 //#pragma execution_character_set("GB2312") 这个头定义不需要,但是要注意定义bom头
 
@@ -13,22 +14,42 @@ int main(int argc, char **argv)
         LogI("输出一条日志 %d", i);
     }
 
+    moodycamel::ConcurrentQueue<std::string> queue[4];
+    long long count = 0;
 #pragma omp parallel sections //并行区域
     {
-#pragma omp section //负责这个区域的线程对前面部分进行排序
+#pragma omp section //并行1
         while (true) {
+            if (queue[0].size_approx() == 0)
+                queue[0].enqueue("section0");
         }
-#pragma omp section //负责这个区域的线程对后面部分进行排序
+#pragma omp section //并行2
         while (true) {
+            if (queue[1].size_approx() == 0)
+                queue[1].enqueue("section1");
         }
-#pragma omp section //负责这个区域的线程对后面部分进行排序
+#pragma omp section //并行3
         while (true) {
+            if (queue[2].size_approx() == 0)
+                queue[2].enqueue("section2");
         }
-#pragma omp section //负责这个区域的线程对后面部分进行排序
+#pragma omp section //并行4
         while (true) {
+            if (queue[3].size_approx() == 0)
+                queue[3].enqueue("section3");
         }
-#pragma omp section //负责这个区域的线程对后面部分进行排序
+#pragma omp section //并行5 （实际实验count的增长是每秒100W次左右）
         while (true) {
+            for (size_t i = 0; i < 4; i++) {
+                std::string msg;
+                if (queue[i].try_dequeue(msg)) {
+                    count++;
+                    if (count % 1000000 == 0) {
+                        printf("count = %lld\r\n", count);
+                    }
+                }
+                //printf("%s\r\n",msg.c_str());
+            }
         }
     }
     return 0;
